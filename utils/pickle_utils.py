c640ba0e30d282107c1c436f5709ebbe21d6e530
Mohammad Anvaari <anvaari@proton.me>
2023-11-07 22:31:21 +0330

    Add pickle utils
    Add functions to save and load from root path of projet

---
+++ utils/pickle_utils.py
@@ -0,0 +1,36 @@
+import pickle 
+import os 
+import pathlib
+import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+
+log_level = os.getenv('LOG_LEVEL','info').upper()
+script_path=os.path.dirname(os.path.abspath(__file__))
+project_path = str(pathlib.Path(script_path).parent.absolute())
+
+logger = logging.getLogger(__name__)
+logger.setLevel(log_level)
+
+def save_as_pickle(python_object:list|dict|tuple,file_name:str) -> None:
+    with open(os.path.join(project_path,file_name),'wb') as fp:
+        try:
+            pickle.dump(python_object,fp)
+        except Exception as e:
+            logger.error(f"Can't save {file_name} "
+                         f"file as picke.\n{e}",exc_info=True)
+        else:
+            logger.debug("Save picke file successfully")
+
+def load_dict_pickle(file_name:str) -> dict:
+    if not os.path.isfile(os.path.join(project_path,file_name)):
+        return dict()
+    with open(os.path.join(project_path,file_name),'wb') as fp:
+        try:
+            python_object = pickle.load(fp)
+        except Exception as e:
+            logger.error(f"Can't load {file_name} "
+                         f"file as picke.\n{e}",exc_info=True)
+        else:
+            logger.debug("picke file loaded successfully")
