39f6fd5cad90fc890e0f911b88aaacac9c72eb91
Mohammad Anvaari <anvaari@proton.me>
2023-11-08 01:05:47 +0330

    Get values of BackOffConf from environment

--- configs/configs.py
+++ configs/configs.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
+from os import getenv
 
 @dataclass(frozen=True)
 class BackOffConfs:
-    max_restart : int = 7
-    exponential_ratio : int = 1
+    max_restart : int = int(getenv("MAX_RESTART",7))
+    exponential_ratio : int = int(getenv("EXPONENTIAL_RATIO",1))
 
 
