683052cd5a8dee5de0528729a7396a7d5fb08862
Mohammad Anvaari <anvaari@proton.me>
2023-11-10 00:24:06 +0330

    Add is_enabled to BackOffConfs

--- configs/configs.py
+++ configs/configs.py
@@ -5,5 +5,6 @@ from os import getenv
 class BackOffConfs:
     max_restart : int = int(getenv("MAX_RESTART",7))
     exponential_ratio : int = int(getenv("EXPONENTIAL_RATIO",1))
+    is_enabled : bool = bool(int(getenv("ENABLE_BACKOFF",1)))
 
 
