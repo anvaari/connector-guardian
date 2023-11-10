f87ec15c8c2be9b80d8ced603aac0b51bb6e18ba
Mohammad Anvaari <mohamad.anvari@snapp.cab>
2023-11-10 14:49:34 +0330

    Change default value of EXPONENTIAL_RATIO to 2

--- configs/configs.py
+++ configs/configs.py
@@ -4,7 +4,7 @@ from os import getenv
 @dataclass(frozen=True)
 class BackOffConfs:
     max_restart : int = int(getenv("MAX_RESTART",7))
-    exponential_ratio : int = int(getenv("EXPONENTIAL_RATIO",1))
+    exponential_ratio : int = int(getenv("EXPONENTIAL_RATIO",2))
     is_enabled : bool = bool(int(getenv("ENABLE_BACKOFF",1)))
 
 
