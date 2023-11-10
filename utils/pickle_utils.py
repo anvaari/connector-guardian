635c10a5be8b4d493216f94e088512f1be338551
Mohammad Anvaari <mohamad.anvari@snapp.cab>
2023-11-10 15:58:56 +0330

    Fix error in using |
    It is for python3.10 but python3.9 is used for Guardian

--- utils/pickle_utils.py
+++ utils/pickle_utils.py
@@ -2,6 +2,7 @@ import pickle
 import os 
 import pathlib
 import logging
+from typing import Union
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,7 +14,7 @@ project_path = str(pathlib.Path(script_path).parent.absolute())
 logger = logging.getLogger(__name__)
 logger.setLevel(log_level)
 
-def save_as_pickle(python_object:list|dict|tuple,file_name:str) -> None:
+def save_as_pickle(python_object:Union[list,dict,tuple],file_name:str) -> None:
     with open(os.path.join(project_path,file_name),'wb') as fp:
         try:
             pickle.dump(python_object,fp)
@@ -32,7 +33,7 @@ def load_dict_pickle(file_name:str) -> dict:
         except Exception as e:
             logger.error(f"Can't load {file_name} "
                          f"file as picke, empty dict "
-                         "will be returend.\n{e}",exc_info=True)
+                         "will be returned.\n{e}",exc_info=True)
             return dict()
         else:
             logger.debug("picke file loaded successfully")
